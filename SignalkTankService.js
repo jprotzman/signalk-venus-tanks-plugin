bb139a25308becdb0aa51a396446c4680a896900
Paul Reeve <preeve@pdjr.eu>
2021-02-11 21:48:43 +0000

    Added code comments

--- SignalkTankService.js
+++ SignalkTankService.js
@@ -1,7 +1,37 @@
-dbus = require('dbus-native');
+/**********************************************************************
+ * Copyright 2021 Paul Reeve <paul@pdjr.eu>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 
+ dbus = require('dbus-native');
+
+/**********************************************************************
+ * SignalkTankService implements a 'tank' class dbus service which
+ * satisfies the requirements of the Venus OS and which can be used to
+ * write tank data onto the bus in a format that can be used by the
+ * CCGX display and other Venus OS GUIs.
+ */
 module.exports = class SignalkTankService {
 
+    /******************************************************************
+     * Construct a new SignalkTankService instance and attempt to
+     * connect to the host system dbus, throwing an exception if the
+     * connection fails. Note that this constructor does not create an
+     * actual dbus service for the Signal K tank identified by
+     * fluidtype and tankinstance, a subsequent call to createService()
+     * is required to do that.
+     */
     constructor(fluidtype, tankinstance) {
         this.fluidtype = fluidtype;
         this.tankinstance = tankinstance;
@@ -15,6 +45,12 @@ module.exports = class SignalkTankService {
         if (!this.bus) throw "error connecting to system bus";
     }
 
+    /******************************************************************
+     * createService() attempts to asynchronously instantiate and
+     * initialise a dbus service for this tank. The service is
+     * configured in a way which satisfies the requirements of Venus OS.
+     * An exception is thrown on error.
+     */
     createService() {
         if (this.bus) {
             this.bus.requestName(this.servicename, 0x4, function (err, retcode) {
@@ -62,6 +98,10 @@ module.exports = class SignalkTankService {
         }
     }
             
+    /******************************************************************
+     * update(currentlevel[, capacity]) updates the dbus service for
+     * this tank from the supplied data.
+     */
     update(currentlevel, capacity=null) {
         if ((this.bus) && (this.iface)) {
             if (capacity) {
