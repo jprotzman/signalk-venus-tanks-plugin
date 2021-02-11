2752fde7c20114ba902cbd1c297a5c49b4fd8200
Paul Reeve <preeve@pdjr.eu>
2021-02-11 22:17:39 +0100

    Lots of debugging

--- SignalkTankService.js
+++ SignalkTankService.js
@@ -1,30 +1,32 @@
-module exports = class SignalkTankService {
+dbus = require('dbus-native');
+
+module.exports = class SignalkTankService {
 
     constructor(fluidtype, tankinstance) {
         this.fluidtype = fluidtype;
         this.tankinstance = tankinstance;
         this.tankcapacity = 0.0;
-        this.servicename = "com.victronenergy.tank.signalk_tank_" + this.fluidtype + "_" + this.instance;
+        this.servicename = "com.victronenergy.tank.signalk_tank_" + this.fluidtype + "_" + this.tankinstance;
         this.interfacename = this.servicename;
-        this.objectpath = this.servicename.replaceAll('.', '/');
+        this.objectpath = this.servicename.replace(/\./g, '/');
         this.bus = dbus.systemBus();
         this.ifacedesc = null;
         this.iface = null;
         if (!this.bus) throw "error connecting to system bus";
     }
 
-    async createService() {
+    createService() {
         if (this.bus) {
-            const requestNamePromise = util.promisify(this.bus.requestName);
-            requestNamePromise(this.servicename, 0x4)
-            .then(retcode => {
-                if (retcode === 1) {
+            this.bus.requestName(this.servicename, 0x4, function (err, retcode) {
+                if ((err) || (retcode !== 1)) {
+                    throw "service creation failed (" + (err)?err:retcode + ")";
+                } else {
                     this.ifacedesc = {
                         name: this.interfacename,
                         properties: {
                             '/Mgmt/ProcessName': 's',
                             '/Mgmt/ProcessVersion': 's',
-                            '/Mgmt/Connection': 's'
+                            '/Mgmt/Connection': 's',
                             '/DeviceInstance': 'i',
                             '/ProductId': 's',
                             '/ProductName': 's',
@@ -51,27 +53,26 @@ module exports = class SignalkTankService {
                         '/FluidType': this.fluidtype,
                         '/Capacity': this.tankcapacity,
                         '/Remaining': 0
-                    }
+                    };
                     this.bus.exposeInterface(this.iface, this.objectpath, this.ifacedesc); 
-                } else {
-                    throw "service creation failed with code = " + retcode;
                 }
-            })
-            .catch(err => {
-                throw "service creation failed (" + err + ")";
-            });
+            }.bind(this));
+        } else {
+            throw "not connected to system bus";
         }
     }
             
     update(currentlevel, capacity=null) {
-        if (capacity) {
-            this.tankcapacity = capacity;
-            this.iface['/Capacity'] = capacity;
+        if ((this.bus) && (this.iface)) {
+            if (capacity) {
+                this.tankcapacity = capacity;
+                this.iface['/Capacity'] = capacity;
+            }
+            if (this.tankcapacity) {
+                this.iface['/Remaining'] = (this.tankcapacity * currentlevel);
+            }
+            this.iface['/Level'] = Math.round(currentlevel * 100);
         }
-        if (this.tankcapacity) {
-            this.iface['/Ramaining'] = (this.tankcapacity * currentlevel);
-        }
-        this.iface['/Level'] = Math.round(currentlevel * 100);
     }
 
 }
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-/*
+/**********************************************************************
  * Copyright 2021 Paul Reeve <paul@pdjr.eu>
- * Portions Copyright (2017) Scott Bender (see https://github.com/sbender9/signalk-simple-notifications)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -10,22 +9,30 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
  */
 
 const Log = require('./lib/signalk-liblog/Log.js');
 const SignalkTankService = require('./SignalkTankService');
 
 const PLUGIN_ID = "pdjr-skplugin-venus-tanks";
-const PLUGIN_SCHEMA_FILE = _dirname + "/schema.json";
-const PLUGIN_UISCHEMA_FILE = __dirname + "/uischema.json";
+const N2K_FLUID_TYPES = {
+    fuel: 0,
+    freshWater: 1,
+    greyWater: 2,
+    liveWell: 3,
+    lubrication: 4,
+    wasteWater: 5,
+    gasoline: 6,
+    error: 14,
+    unavailable: 15
+};
 
 module.exports = function(app) {
     var plugin = {};
     var unsubscribes = [];
-    var fluidTypes = { "fuel": 0, "freshWater": 1, "wasteWater": 5 };
 
     plugin.id = PLUGIN_ID;
     plugin.name = "Venus tank ";
@@ -52,37 +59,39 @@ module.exports = function(app) {
     plugin.start = function(options) {
         // If no tank paths are specified, then recover all available paths
         // from the server.
-        if (options.tanks.length == 0) {
+        if ((!options.tanks) || (options.tanks.length == 0)) {
             var tanks = app.streambundle.getAvailablePaths().filter(p => p.startsWith('tanks.')).reduce((a,v) => {
                 var matches;
-                if (matches = v.match(/^(.*\..*\..*)\..*/)) a.add(matches[0]);
+                if (matches = v.match(/^(tanks\..*\..*)\..*/)) a.add(matches[1]);
                 return(a);
             }, new Set());
             options.tanks = Array.from(tanks);
         }
 
+        console.log(JSON.stringify(options.tanks));
+
         options.tanks.forEach(tank => {
             var parts = tank.split(/\./);
             if (parts.length == 3) {
-                let fluidType = (N2K_FLUID_TYPES[parts[1]])?N2K_FLUID_TYPES[parts[1]]:0;
+                let fluidType = (N2K_FLUID_TYPES[parts[1]])?N2K_FLUID_TYPES[parts[1]]:N2K_FLUID_TYPES['unavailable'];
                 let instance = parts[2];
                 let capacity = null;
                 let tankService = null;
                 try {
                     tankService = new SignalkTankService(fluidType, instance);
-                    await tankService.createService();
+                    createService(tankService);
                     var stream = app.streambundle.getSelfStream(tank + ".currentLevel");
                     if (stream) {
                         unsubscribes.push(stream.onValue(currentLevel => {
-                            if (!capacity) capacity = app.getSelfPath(rule.tankpath + ".capacity.value");
+                            if (!capacity) capacity = app.getSelfPath(tank + ".capacity.value");
                             tankService.update(currentLevel, capacity)
-                        });
+                        }));
                     }
                 } catch(e)  {
-                    Log.E("unable to create service for %s (%s)", tank, e);
+                    log.E("unable to create service for %s (%s)", tank, e);
                 }
             } else {
-                Log.E("invalid tank path (%s)", tank);
+                log.E("invalid tank path (%s)", tank);
             }
         });
     }
@@ -90,6 +99,10 @@ module.exports = function(app) {
     plugin.stop = function() {
         unsubscribes.forEach(f => f())
         unsubscribes = []
+    }
+
+    async function createService(tankService) {
+        await tankService.createService();
     }
 
     return(plugin);
